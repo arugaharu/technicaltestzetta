bab8ce50eb36352e467e5f07cd1e4d1f3e5499ef
arugaharu <126941113+arugaharu@users.noreply.github.com>
2024-08-19 11:16:14 +0700

    Add files via upload

---
+++ technical.js
@@ -0,0 +1,28 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+
+function result(sentences) {
+  let maxWords = 0;
+
+  sentences.forEach(sentence => {
+    const wordCount = sentence.split(' ').length;
+    if (wordCount > maxWords){
+        maxWords = wordCount;
+    }
+  });
+  return maxWords;
+}
+
+const sentences = [
+    'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+    'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+    'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+  ];
+
+console.log(result(sentences));
+
